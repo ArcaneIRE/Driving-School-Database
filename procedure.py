08911601aa04c0e219085bbab663b89e9158b31f
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2023-11-12 10:49:08 +0000

    Finish procedure.py

--- procedure.py
+++ procedure.py
@@ -4,6 +4,7 @@ import pandas as pd
 from psycopg2 import Error
 from datetime import datetime
 try:
+    print("=== Please log in to database ===")
     connection = psycopg2.connect(
         host="147.252.250.51",
         user="D22126022",
@@ -11,23 +12,61 @@ try:
         port="5432",
         database="postgres")
     cursor = connection.cursor()
-    select_query = 'select * from "Drive856A".timetableslots;'
+    select_query = 'select * from "Drive856A".bookingtimeslot;'
     cursor.execute(select_query)
-    dataframe = pd.DataFrame(
+    df = pd.DataFrame(
         cursor.fetchall(),
-        columns=['', '', '', '', '', '', '', ''])
-    print(dataframe)
+        columns=['ID', 'Start Time', 'End Time', 'Booking ID', 'Staff ID'])
+    print("=== Timeslot Table (Initial) ===")
+    print(df)
+
+    print("=== Add a New Timeslot ===")
+    print("Enter the timeslot details: ")
+    id = input("ID: ")
+    start_time = input("Start Time: ")
+    end_time = input("End Time: ")
+    booking_id = input("Booking ID (can be empty): ")
+    staff_id = input("Staff ID: ")
+
+    if id == "":
+        raise Exception("ID cannot be empty")
+    if start_time == "":
+        raise Exception("Start time cannot be empty")
+    if end_time == "":
+        raise Exception("End time cannot be empty")
+    if booking_id == "":
+        booking_id = "NULL"
+    else:
+        booking_id = f"\'{booking_id}\'"
+    if staff_id == "":
+        raise Exception("Staff ID cannot be empty")
 
     cursor.execute(
-        """ call "REM856A".add_timeslot(11, '14:30', '15:30', NULL, 4); """)
+        f"""call "Drive856A".add_timeslot('{id}', '{start_time}', '{end_time}', {booking_id}, '{staff_id}');""")
     connection.commit()
-    print("Result = ", cursor.fetchall())
+
+    select_query = 'select * from "Drive856A".bookingtimeslot;'
+    cursor.execute(select_query)
+    df = pd.DataFrame(
+        cursor.fetchall(),
+        columns=['ID', 'Start Time', 'End Time', 'Booking ID', 'Staff ID'])
+    print("=== Timeslot Table (Post Query) ===")
+    print(df)
+
+    select_query = 'select * from "Drive856A".timeslot_log;'
+    cursor.execute(select_query)
+    df = pd.DataFrame(
+        cursor.fetchall(),
+        columns=['ID', 'Username', 'Staff ID', 'Date'])
+    print("\n\n")
+    print("=== Log Table (Post Query) ===")
+    print(df)
 except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL", error)
 finally:
     if (connection):
         cursor.close()
         connection.close()
-        print("PostgreSQL connection is closed")
+        print("\n\nPostgreSQL connection is closed")
     else:
         print("Terminating")
